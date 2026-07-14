246f1adfa02bff15009c771d2a376d834ae55a3e
itdaniel-Gumball <itdaniel@kics.sch.id>
2026-07-14 11:46:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    apiUrl: "https://script.google.com/macros/s/AKfycbyZW37lkZ69RFqJeNJKQXHlI3W4xEKxesaxUezOvHQF-BNrP1ebIdUM5-gdM4ufjXAk/exec"
+  apiUrl: "https://script.google.com/macros/s/AKfycbyZW37lkZ69RFqJeNJKQXHlI3W4xEKxesaxUezOvHQF-BNrP1ebIdUM5-gdM4ufjXAk/exec"
 };
